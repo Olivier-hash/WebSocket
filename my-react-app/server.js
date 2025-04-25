74001e95d2e6ba398ec601e3404320abbcec1e88
olivier Rukundo <rr.olivier01@gmail.com>
2025-04-25 22:48:44 +0200

    the websocket added in server.js

---
+++ my-react-app/server.js
@@ -0,0 +1,22 @@
+// webSocket Server.js
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+server.listen(3001, () => {
+  console.log("WebSocket server running on ws://localhost:3001");
+});
